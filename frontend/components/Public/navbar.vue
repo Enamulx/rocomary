c3b20445d0795f8d5c46a0efa5186eb80915ab0b
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-07-15 12:30:30 +0600

    Folder Structure changed

--- frontend/components/Public/navbar.vue
+++ frontend/components/Public/navbar.vue
@@ -1,4 +1,21 @@
-<script setup></script>
+<script setup>
+onMounted(() => {
+  var toggleOpen = document.getElementById("toggleOpen");
+  var toggleClose = document.getElementById("toggleClose");
+  var collapseMenu = document.getElementById("collapseMenu");
+
+  function handleClick() {
+    if (collapseMenu.style.display === "block") {
+      collapseMenu.style.display = "none";
+    } else {
+      collapseMenu.style.display = "block";
+    }
+  }
+
+  toggleOpen.addEventListener("click", handleClick);
+  toggleClose.addEventListener("click", handleClick);
+});
+</script>
 
 <template>
   <header class="shadow-md font-[sans-serif] tracking-wide relative z-50">
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup></script>
 
 <template>
-  <div class="min-h-screen"><</div>
+  <div class="min-h-screen"></div>
 </template>
