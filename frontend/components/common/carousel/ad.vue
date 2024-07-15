b3ab2c33bbe8d9e240f38aaf6bf21eb7f009a1e6
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-07-15 15:32:51 +0600

    home carousel added and make that reusable componnet and recently viewed als added

---
+++ frontend/components/common/carousel/ad.vue
@@ -0,0 +1,33 @@
+<template>
+  <UCarousel
+    ref="carouselRef"
+    v-slot="{ item }"
+    :items="items"
+    :ui="{ item: 'basis-full sm:basis-1/3 md:basis-1/4 md:basis-1/6' }"
+    class="rounded-lg overflow-hidden"
+    arrows
+  >
+    hello
+    <img :src="item" class="w-full h-64" draggable="false" />
+  </UCarousel>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, defineProps } from "vue";
+
+const props = defineProps<{ items: string[] }>();
+
+const carouselRef = ref();
+
+onMounted(() => {
+  setInterval(() => {
+    if (!carouselRef.value) return;
+
+    if (carouselRef.value.page === carouselRef.value.pages) {
+      return carouselRef.value.select(0);
+    }
+
+    carouselRef.value.next();
+  }, 3000);
+});
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,19 @@
-<script setup></script>
+<script setup lang="ts">
+const carouselItems = [
+  "/home/carousel/home-carousel1.png",
+  "/home/carousel/home-carousel2.png",
+  "/home/carousel/home-carousel3.png",
+  "/home/carousel/home-carousel4.png",
+  "/home/carousel/home-carousel5.png",
+  "/home/carousel/home-carousel6.png",
+  "/home/carousel/home-carousel7.png",
+];
+</script>
 
 <template>
-  <div class="min-h-screen"></div>
+  <UContainer>
+    <div class="py-4">
+      <CommonCarouselAd :items="carouselItems" />
+    </div>
+  </UContainer>
 </template>
